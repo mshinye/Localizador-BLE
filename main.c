39c7b828696cb3bda96ed9cc8db5965a1b137116
mshinye <mshinye@gmail.com>
2016-10-02 14:44:44 -0300

    Adiçao de Funcao CountSubstr

--- main.c
+++ main.c
@@ -3,14 +3,67 @@
 #include <errno.h>
 #include <wiringSerial.h>
 
+int fd;
+
+int countSubstr(char substr[5],char strtst[1000])
+{
+    int len1,count,i,j;
+    char *p;
+
+    len1=strlen(strtst);
+    p=substr;
+
+    while ((p = strstr(p, strtst)) != NULL && p != strtst)
+    {
+        count++;
+        p += len1;
+    }
+
+    printf("Found %d occurrences of %s in %s\n", count, s1, s2);
+    return count;   
+}
+
+char* getstr(char* buff)
+{
+    int i = 0;
+    int j = 0;
+
+    //serialFlush(fd);
+
+    while(!serialDataAvail(fd) && i<1000)
+    {
+        i++;
+        delay(10);
+    }
+
+    if (i== 1000)
+    {
+        printf("sem dados no serial\n");
+        buff = "erro";
+        return(buff);
+    }
+
+    while(serialDataAvail(fd)>0)
+    {
+        delay(10);
+        buff[j] = serialGetchar(fd);
+        j++;
+    }
+    buff[j] = 0;
+
+    printf("%s \n", buff); //debug
+
+    return(buff);
+}
+
+
 int main ()
 {
-	int fd ;
 	int b = 0;
 	int a = 0;
 	int i = 0;
-char mac[] = "78A5048C47AF"; //green module mac
-char check[100] = "\0";
+
+char check[1000] = "\0";
 
 if ((fd = serialOpen ("/dev/ttyAMA0", 9600)) < 0)
 {
@@ -19,32 +72,14 @@ if ((fd = serialOpen ("/dev/ttyAMA0", 9600)) < 0)
 }
 
 fflush(stdout);
-serialPuts (fd,"AT+START"); //serve para acordar o modulo
-delay(500);
-serialFlush (fd) ;
-printf("go\n");
-
-fflush(stdout);
 serialPuts (fd,"AT+DISI?"); //serve para acordar o modulo
+getstr(check);
 delay(500);
+getstr(check);
 serialFlush (fd) ;
 printf("go\n");
 
 
-
-
-while(serialDataAvail(fd)==0); //espera a chegada de algum byte na porta serial
-
-//o bloco abaixo recebe as informacoes do buffer e copia para a string check
-i=0;
-while((serialDataAvail(fd))!=0)
-{
-	delay(10);
-	check[i] = serialGetchar(fd);
-	i++;
-}
-check[i] = 0;
 //fim do bloco que copia os dados para a string check
-
-printf("%s \n", check); //debug  
+  
 }  
