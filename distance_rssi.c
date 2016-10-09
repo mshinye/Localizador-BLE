e14caf4bc44a1c80da33e93280af77fd1e5f194c
Marcelo <you@example.com>
2016-10-09 20:27:54 +0000

    "Atualizacoes Codigo de Estimador de distancia, realizando mediana dos valores obtidos."

--- distance_rssi.c
+++ distance_rssi.c
@@ -3,18 +3,18 @@
   A is the RSSI value equivalent of distance of 1 meter between transmitter and receiver
 
   distance = 10^-(RSSI + A)/(10 * n)
-*/
+  */
 #include "distance_rssi.h"
 #include <math.h>
 
 double getDistance_RSSI(double rssi, double a) {
 
-	double ratio = rssi*1.0/a;
-  	if (ratio < 1.0) {
-    		return pow(ratio,10);
-	}
-  	else {
-    		double accuracy =  (0.89976)*pow(ratio,7.7095) + 0.111;    
-    	return accuracy;
-  	}
+    double ratio = rssi*1.0/a;
+    if (ratio < 1.0) {
+        return pow(ratio,10);
+    }
+    else {
+        double accuracy =  (0.89976)*pow(ratio,7.7095) + 0.111;    
+        return accuracy;
+    }
 }
